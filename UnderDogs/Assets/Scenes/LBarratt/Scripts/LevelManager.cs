e1c5f094f763cefd627b92db0cffd1a418309c21
Luke B <1018550@student.sae.edu.au>
2021-11-09 10:54:12 +0800

    Implementation of Basic UI #1
    - Added Text Mesh Pro To Project
    - Added a loading screen
    - Created a new scene in LBarratt's Folder

---
+++ UnderDogs/Assets/Scenes/LBarratt/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance;
+
+    [SerializeField] private GameObject _loaderCanvas;
+    [SerializeField] private Image _progressBar;
+    
+    // Youtube (Tarodev) Tutorial Link: https://www.youtube.com/watch?v=OmobsXZSRKo
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public async void LoadScene(string sceneName)
+    {
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+        scene.allowSceneActivation = false;
+
+        _loaderCanvas.SetActive(true);
+
+        do
+        {
+            _progressBar.fillAmount = scene.progress;
+        } while (scene.progress < 0.9f);
+
+        scene.allowSceneActivation = true;
+        _loaderCanvas.SetActive(false);
+    }
+}
