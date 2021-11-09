74c23902d9c01ae9282c58c8883c6072b44c8282
Dylan-Bello <66900945+Dylan-Bello@users.noreply.github.com>
2021-11-09 13:00:07 +0800

    Finished Basic Event System

---
+++ UnderDogs/Assets/Scenes/DBello/Scripts/PressurePlate.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    [SerializeField] GameEvent _onActivate;
+    [SerializeField] GameEvent _onDeactivate;
+    public GameObject mechanism;
+
+    public bool _activated;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_activated == false)
+            Activate();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (_activated == true)
+            Deactivate();
+    }
+
+
+    void Activate()
+    {
+        _onActivate?.Invoke();
+        Debug.Log("Activated");
+        _activated = true;
+    }
+
+    private void Deactivate()
+    {
+        _onDeactivate.Invoke();
+        Debug.Log("Deactivated");
+        _activated = false;
+    }
+}
