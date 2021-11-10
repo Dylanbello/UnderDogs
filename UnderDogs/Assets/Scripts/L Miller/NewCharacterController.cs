856595f394101d6c83848709f027d1f6220abe36
LukeMiller1017576 <51160845+LukeMiller1017576@users.noreply.github.com>
2021-11-10 14:34:44 +0800

    NewCharacterControllerScript created

---
+++ UnderDogs/Assets/Scripts/L Miller/NewCharacterController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCharacterController : MonoBehaviour
+{
+    public Action<Vector2> onMovementInput;
+    public Action<Vector3> onMovementDirectionInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetMovementInput();
+        GetMovementDirection();
+    }
+
+    private void GetMovementDirection()
+    {
+        var cameraForwardDirection = Camera.main.transform.forward;
+    }
+
+    private void GetMovementInput()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        onMovementInput?.Invoke(input);
+    }
+}
