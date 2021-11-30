7f08e437658a9c26e28869b83b88265c97a123ec
Dylan-Bello <66900945+Dylan-Bello@users.noreply.github.com>
2021-11-30 10:18:47 +0800

    New Pressure Plate Script

--- UnderDogs/Assets/Scenes/DBello/Scripts/Mechanisms/PressurePlate.cs
+++ UnderDogs/Assets/Scenes/DBello/Scripts/Mechanisms/PressurePlate.cs
@@ -2,39 +2,62 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PressurePlate : MonoBehaviour
 {
-    [SerializeField] GameEvent _onActivate;
-    [SerializeField] GameEvent _onDeactivate;
+    [SerializeField] UnityEvent plateActivation;
+    [SerializeField] UnityEvent plateDeactivation;
+    //[SerializeField] GameEvent _onActivate;
+    //[SerializeField] GameEvent _onDeactivate;
     //public GameObject mechanism;
 
-    public bool _activated;
+    //public bool _activated;
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (_activated == false)
+    //        Activate();
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (_activated == true)
+    //        Deactivate();
+    //}
+
+
+    //void Activate()
+    //{
+    //    _onActivate?.Invoke();
+    //    Debug.Log("Activated");
+    //    _activated = true;
+    //}
+
+    //void Deactivate()
+    //{
+    //    _onDeactivate?.Invoke();
+    //    Debug.Log("Deactivated");
+    //    _activated = false;
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
-        if (_activated == false)
-            Activate();
+        //Check which character is on the pressure plate.
+        //or check objects weight, etc.
+        
+
+            if (other.CompareTag("character1")) { plateActivation.Invoke(); }
+        else return;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (_activated == true)
-            Deactivate();
-    }
+        //Check which character is on the pressure plate.
+        //or check objects weight, etc.
+       
 
-
-    void Activate()
-    {
-        _onActivate?.Invoke();
-        Debug.Log("Activated");
-        _activated = true;
-    }
-
-    void Deactivate()
-    {
-        _onDeactivate?.Invoke();
-        Debug.Log("Deactivated");
-        _activated = false;
+            if (other.CompareTag("character1")) { plateDeactivation.Invoke(); }
+        else return;
     }
 }
