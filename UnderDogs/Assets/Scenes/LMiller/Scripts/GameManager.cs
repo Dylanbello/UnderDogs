37f68b173c0610a1e873bfb514d795e4c39c7e02
LukeMiller1017576 <51160845+LukeMiller1017576@users.noreply.github.com>
2022-02-10 14:31:19 +0800

    Game Manager

---
+++ UnderDogs/Assets/Scenes/LMiller/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.LogError("Game Manager is Null");
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public enum GameState
+    {
+        MainMenu,
+        Cutscene,
+        Playing,
+        Paused,
+        GameOver
+    }
+}
