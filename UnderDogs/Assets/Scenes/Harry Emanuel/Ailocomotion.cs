3ec1b107c296f34bcdf2f3a5346d98346c4fb186
Harry <harry.emmanuel.02@gmail.com>
2022-02-24 11:16:20 +0800

    First Iteration AI | Player Location and Animation
                                                             [TheKiwiCoder tutorial]
    -Simple player location script
    -Animtion blender tree for speed
    -Nav baked ground
    -Performance for player location

---
+++ UnderDogs/Assets/Scenes/Harry Emanuel/Ailocomotion.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ailocomotion : MonoBehaviour
+{
+    public float maxTime, maxDistance = 1.0f;
+    float timer = 0.0f;
+
+    NavMeshAgent agent;
+
+    public Transform playerTransform;
+    Animator animator;
+
+    void Start()
+    {
+        
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        timer-= Time.deltaTime;
+        if(timer < 0.0f)
+        {
+            float sgdistance = (playerTransform.position - agent.destination).sqrMagnitude;
+            if(sgdistance > maxDistance*maxDistance)
+            {
+                agent.destination = playerTransform.position;
+            }
+            timer = maxTime;
+
+            agent.destination = playerTransform.position;
+        }
+
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+    }
+}
