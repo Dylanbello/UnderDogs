c755de220944c99ea8f05890b407aedbe48773f0
LukeMiller1017576 <51160845+LukeMiller1017576@users.noreply.github.com>
2022-03-09 12:29:58 +0800

    Spinning Cog Script

---
+++ UnderDogs/Assets/Scenes/LMiller/Scripts/CogCollectible.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class CogCollectible : MonoBehaviour
+{
+    public int cogValue;
+    public float cogSpinSpeed;
+
+    //Reference to UI;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0f, 0f, Time.deltaTime * cogSpinSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerInput>()) 
+        {
+            //Increment UI
+            Destroy(this.gameObject);
+        }
+    }
+}
