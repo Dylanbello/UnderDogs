2dd93f2123df2ed93ea5fcb7499a88f4c9fc4cd5
Ewatso <56715011+Ewatso@users.noreply.github.com>
2022-03-18 12:03:39 +0800

    EthanPushMinimap1

---
+++ UnderDogs/Assets/Scenes/Ethan Watson/MultipleTargetCamera.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleTargetCamera : MonoBehaviour
+{
+    public List<Transform> targets;
+
+    public Vector3 offset;
+    private Vector3 velocity;
+    public float smoothTime = .5f;
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+
+    public float zoomLimiter = 50f;
+
+    private Camera cam;
+    void LateUpdate()
+    {
+        if (targets.Count == 0)
+            return;
+
+        move();
+        zoom();
+
+    }
+
+    void zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    }
+    void move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPosition = centerPoint + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+
+        }
+
+        return bounds.size.x;
+    }
+    Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+
+        }
+
+        return bounds.center;
+    }
+}
