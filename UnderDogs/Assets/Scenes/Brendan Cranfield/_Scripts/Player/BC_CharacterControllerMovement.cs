e2494d3a971190de1020ccbd2a72b6a3ab69315c
Hartilin <1018127@student.sae.edu.au>
2022-04-08 15:01:54 +0800

    Removed Double Jump

--- UnderDogs/Assets/Scenes/Brendan Cranfield/_Scripts/Player/BC_CharacterControllerMovement.cs
+++ UnderDogs/Assets/Scenes/Brendan Cranfield/_Scripts/Player/BC_CharacterControllerMovement.cs
@@ -10,10 +10,12 @@ public class BC_CharacterControllerMovement : MonoBehaviour
     Transform cam;
     CinemachineFreeLook cmFreeLook;
     Animator animator;
+    Rigidbody rigidbody;
 
     Vector3 playerVelocity;
     [HideInInspector] public Vector3 moveInput;
     [HideInInspector] public bool isSprinting = false;
+    public bool grounded;
     
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float sprintSpeed = 5f;
@@ -23,6 +25,7 @@ public class BC_CharacterControllerMovement : MonoBehaviour
 
     private void Awake()
     {
+        //rigidbody = GetComponent<Rigidbody>();
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
         cam = GetComponentInChildren<Camera>().transform;
@@ -31,12 +34,13 @@ public class BC_CharacterControllerMovement : MonoBehaviour
 
     private void Start()
     {
+        cam.parent = null;
         cmFreeLook.Follow = transform;
         cmFreeLook.LookAt = transform;
 
         if(Cursor.visible) { Cursor.lockState = CursorLockMode.Confined; Cursor.visible = false; }
 
-        canJump = true;
+        //canJump = true;
     }
 
     private void Update()
@@ -45,9 +49,10 @@ public class BC_CharacterControllerMovement : MonoBehaviour
         Movement();
         Rotation();
         HandleSprinting();
+        SetCorrectAnimation();
+        isGrounded();
 
-
-        animator.SetFloat("Move", SetCorrectAnimation());
+        //if(isGrounded) animator.ResetTrigger("Jump");
     }
 
     public void OnEnableControls() { GetComponent<PlayerInput>().enabled = true; }
@@ -67,68 +72,69 @@ public class BC_CharacterControllerMovement : MonoBehaviour
     #region Movement & Falling
     void Movement()
     {
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y).normalized;
         move = cam.transform.forward * move.z + cam.transform.right * move.x;
         move.y = 0;
-
+        
         controller.Move(move * Time.deltaTime * moveSpeed);
     }
 
     void Rotation()
     {
         if(moveInput == Vector3.zero) { return; }   //Comment out this line to allow turning while stationary.
-
+        
         float targetAngle = Mathf.Atan2(moveInput.x, moveInput.y) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
         Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
         transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * turnSmoothTime);
+        
     }
 
     void CheckFalling()
     {
-        if (isGrounded) jumpCount = 0;
+        //if (isGrounded) jumpCount = 0;
 
         //Stop Falling
-        if (playerVelocity.y < 0 && isGrounded) { playerVelocity.y = 0; }
+        if (playerVelocity.y < 0 && grounded) { playerVelocity.y = 0; }
 
         //Applies gravity
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }
 
-    bool isGrounded { get { return Physics.Raycast(transform.position, Vector3.down, .1f, 1 << LayerMask.NameToLayer("Environment")); } }
-
+    public void isGrounded()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, .1f, 1 << LayerMask.NameToLayer("Environment"))) { grounded = true; }
+        else { grounded = false; }
+    }
     #endregion
 
     #region Jumping
 
-    bool canJump;
-    int jumpCount;
+    //bool canJump = true;
 
     public void Jump()
     {
-        if (jumpCount == 1 || !canJump) return;     //Guard clause for double jumping.
+        if (!grounded) return;     //Guard clause for double jumping.
         //animator.SetTrigger("Jump");
 
-        canJump = false;
-        jumpCount += 1;
-
+        //canJump = false;
+        //jumpCount += 1;
 
         playerVelocity.y += Mathf.Sqrt(jumpHeight * -3 * gravityValue);
-        StartCoroutine(JumpCooldown());
+        //StartCoroutine(JumpCooldown());
     }
-
+    /*
     IEnumerator JumpCooldown()
     {
         yield return new WaitForSeconds(0.2f);
-        //animator.ResetTrigger("Jump");
         canJump = true;
     }
-
+    */
     #endregion
 
     #region Applying Animation Values
 
-    private float SetCorrectAnimation()
+    private void SetCorrectAnimation()
     {
         float charMoveSpeed = 0;
         float moveX = moveInput.x;
@@ -141,9 +147,18 @@ public class BC_CharacterControllerMovement : MonoBehaviour
         else if (moveX > -0.7f || moveY > -0.7f) charMoveSpeed = 0.5f;
 
         //Sprinting
-        //if(isSprinting) charMoveSpeed = 2;
+        if(isSprinting) 
+        { 
+            animator.SetBool("isRunning", true); 
+            animator.SetBool("isWalking", false); 
+        }
+        else 
+        { 
+            animator.SetBool("isRunning", false); 
+            animator.SetBool("isWalking", true); 
+        }
 
-        return charMoveSpeed;
+        animator.SetFloat("Move", charMoveSpeed);
     }
 
     #endregion
