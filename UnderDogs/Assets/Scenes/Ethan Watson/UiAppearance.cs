3e6791bb3977893e82cc77f3827f0a3b2edc182b
Ewatso <56715011+Ewatso@users.noreply.github.com>
2022-04-08 15:14:13 +0800

    Ethan Push In Game Tutorial UI

---
+++ UnderDogs/Assets/Scenes/Ethan Watson/UiAppearance.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiAppearance : MonoBehaviour
+{
+    [SerializeField] private Image customImage;
+
+    void OnTriggerEnter(Collider other)
+    {
+       
+            customImage.enabled = true;
+       
+        
+    }
+    void OnTriggerExit(Collider other)
+    {
+        
+        {
+            customImage.enabled = false;
+        }
+    }
+}
