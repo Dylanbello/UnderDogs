68d90f6a881ec83d06de28de07f91c9829088721
Dylan-Bello <66900945+Dylan-Bello@users.noreply.github.com>
2022-04-23 12:57:05 +0800

    Added - Bridge SFX, Pressure Plate Sfx & Gate Sfx

--- UnderDogs/Assets/Scenes/DBello/Scripts/Mechanisms/Mechanism.cs
+++ UnderDogs/Assets/Scenes/DBello/Scripts/Mechanisms/Mechanism.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class Mechanism : MonoBehaviour
 {
     private Animator animator;
+    private AudioSource audioSource;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
     public void Animate(bool activate)
     {
         Debug.Log("animating" + activate);
         animator.SetBool("Activate", activate);
+
+        if (activate == true)
+        {
+            audioSource.PlayDelayed(.2f);
+        }
     }
 }
