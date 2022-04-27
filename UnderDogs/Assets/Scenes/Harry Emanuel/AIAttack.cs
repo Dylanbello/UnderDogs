e2a42432a5bddee93b8ecc96fe23ca618f56d654
Harry <harry.emmanuel.02@gmail.com>
2022-04-27 09:23:45 +0800

    Animation attack animation working
    -Reverted AI attack script for ani to work again

--- UnderDogs/Assets/Scenes/Harry Emanuel/AIAttack.cs
+++ UnderDogs/Assets/Scenes/Harry Emanuel/AIAttack.cs
@@ -5,51 +5,40 @@ using UnityEngine;
 [AddComponentMenu("AI/AIAttack")]
 public class AIAttack : MonoBehaviour
 {
-    Ailocomotion aiLocomotion;
-    public DogManager dogManager;
+    Ailocomotion aiLocaomotion;
     private float attackTimer;
     [SerializeField] private int attackDamage = 50;
     [SerializeField] private float attackDelay = 1;
     [SerializeField] private Animator ai_Attack;
-    [SerializeField] AudioSource s_attack;
+    [SerializeField] AudioSource as_attack;
     
-    private void Awake() 
-    { 
-        aiLocomotion = GetComponentInParent<Ailocomotion>();
-        s_attack = GetComponent<AudioSource>();
-    }
-
-    private void Update()
-    { 
-        attackTimer += Time.deltaTime;
-        if(aiLocomotion.m_PlayerNear == false)
-        {
-            ai_Attack.SetBool("Attack", false);
-        }
-        
-    }
+    private bool _isDistanceCheck = false;
+    
+    private void Awake() { aiLocaomotion = GetComponent<Ailocomotion>();}
+    
+    private void Update(){ attackTimer += Time.deltaTime;}
 
     private void OnTriggerStay(Collider other)
     {
-        if(!other.TryGetComponent(out dogManager))
-        {
-            ai_Attack.SetBool("Attack", false);
-            s_attack.Stop();
-        }
-        /*
-        if(dogManager.playerHealth.IsDead)
-        {
-            Debug.Log("if the fucking enemy is still attacking im done");
-        }
-        */
+        DogManager dogManager = other.GetComponent<DogManager>();
         if(!dogManager || attackTimer < attackDelay) {return;}
         
         attackTimer = 0;
 
-        s_attack.Play();
-        dogManager.playerHealth.Damage(attackDamage);
-        ai_Attack.SetBool("Attack", true);
-
-        dogManager=null;
+        if(dogManager==true)
+        {
+            as_attack.Play();
+            dogManager.playerHealth.Damage(attackDamage);
+            ai_Attack.SetBool("Attack", true);
+        }
+        else()
+        {
+            ai_Attack.SetBool("Attack", false);
+            as_attack.Stop();
+        
+        }
+        
     }
+
+
 }
